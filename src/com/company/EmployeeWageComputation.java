0297827df0fd384fd471591bc5bf4d136adc2c69
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-08-02 20:05:57 +0100

    UC-8 Completed

--- src/com/company/EmployeeWageComputation.java
+++ src/com/company/EmployeeWageComputation.java
@@ -5,14 +5,11 @@ public class EmployeeWageComputation {
     // Constants
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE = 20;
-    public static final int NUMBER_OF_WORKING_DAYS = 5;
-    public static final int NUMBER_OF_WORKING_HOURS = 10;
 
     /**
      * This is a void method used to calculate the salary of the employee
      */
-    public static void computeWages(){
+    public static void computeWages(String companyName, int empRate, int numberOfWorkingDays, int numberOfWorkingHours){
 
         // Variables
         int empHours = 0;
@@ -22,32 +19,30 @@ public class EmployeeWageComputation {
         int totalDays = 0;
 
         // loop condition to check if the total days are matching number of working days/hours and eventually calculate salary
-        while (totalDays <= NUMBER_OF_WORKING_DAYS || totalHrs <= NUMBER_OF_WORKING_HOURS){
+        while (totalDays <= numberOfWorkingDays || totalHrs <= numberOfWorkingHours){
 
             totalDays++;
+
             // random numbers (0,1,2)
             int randomNum = (int)( Math.random() * 2 + 1);
 
             // selection statement
             if (randomNum == IS_FULL_TIME){
-                System.out.println("Full time - Present");
                 empHours = 8;
             }
             else if (randomNum == IS_PART_TIME){
-                System.out.println("Part time - Present");
                 empHours = 4;
             }
             else{
-                System.out.println("Employee is Absent" + '\n');
+                empHours = 0;
             }
 
             // calculation of salary and hours
             totalHrs+=empHours;
-            salary = EMP_RATE * empHours;
-            System.out.println("Employee salary for a day: " + salary + '\n');
+            salary = empRate * empHours;
             totalWage+=salary;
         }
-        System.out.println("Total salary: " + totalWage);
+        System.out.println("Employee is in: " + companyName + ", Total salary: " + totalWage);
     }
 
     /**
@@ -56,6 +51,9 @@ public class EmployeeWageComputation {
      */
     public static void main(String[] args) {
         System.out.println("Welcome Employee" + "\n");
-        computeWages();
+
+        // calling the method from above with the parameters declared
+        computeWages("Company-A", 20, 5, 10);
+        computeWages("Company-B", 25, 10, 20);
     }
 }
